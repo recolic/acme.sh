35f2cb0976c500763b8beb9154e7c5c0c22f8fba
Bensong Liu <bensl@microsoft.com>
2021-04-27 18:34:00 +0800

    add freemyip.com

--- dnsapi/dns_freemyip.sh
+++ dnsapi/dns_freemyip.sh
@@ -12,9 +12,20 @@ dns_freemyip_add() {
   txtvalue="$2"
 
   _info "Add TXT record using freemyip.com api"
-  _info "WARNING: If your DDNS domain is 'MYDOMAIN.freemyip.com', you can only create certificate for 'MYDOMAIN.freemyip.com' or '*.MYDOMAIN.freemyip.com'. Subdomains are not supported. "
+  _info "WARNING: If your DDNS domain is 'MYDOMAIN.freemyip.com', you can only create certificate for 'MYDOMAIN.freemyip.com' or '*.MYDOMAIN.freemyip.com'. Subdomain is not supported. "
   _debug "fulldomain: $fulldomain"
-  _debug "txtvalue(should be empty on deleting): $txtvalue"
+  _debug "txtvalue: $txtvalue"
+
+  freemyip_token="${freemyip_token:-$(_readaccountconf_mutable freemyip_token)}"
+  if [ -z "$freemyip_token" ]; then
+    freemyip_token=""
+    _err "You don't specify freemyip_token yet."
+    _err "Please specify your token and try again."
+    return 1
+  fi
+
+  #save the credentials to the account conf file.
+  _saveaccountconf_mutable freemyip_token "$freemyip_token"
 
   # txtvalue must be url-encoded. But it's not necessary for acme txt value. 
   _get "https://freemyip.com/update?token=$freemyip_token&domain=$fulldomain&txt=$txtvalue" | grep OK
@@ -30,6 +41,17 @@ dns_freemyip_rm() {
   _info "Delete TXT record using freemyip.com api"
   _debug "fulldomain: $fulldomain"
   _debug "txtvalue: $txtvalue"
+
+  freemyip_token="${freemyip_token:-$(_readaccountconf_mutable freemyip_token)}"
+  if [ -z "$freemyip_token" ]; then
+    freemyip_token=""
+    _err "You don't specify freemyip_token yet."
+    _err "Please specify your token and try again."
+    return 1
+  fi
+
+  #save the credentials to the account conf file.
+  _saveaccountconf_mutable freemyip_token "$freemyip_token"
 
   # Leave the TXT record as empty or "null" to delete the record. 
   _get "https://freemyip.com/update?token=$freemyip_token&domain=$fulldomain&txt=" | grep OK
